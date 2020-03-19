ebbcacf992c93a07d0eb58488c608f99b2c3e6fe
Joseph <josephvisaac@gmail.com>
2020-03-19 12:07:04 -0400

    test

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const Theologians = require('../models/TheologianModel');
 const User = require('../models/User');
 
-router.post('/disptheo', (req, res, next) => {
-    // console.log('-=-=-=-', req.body);
+router.post('/disptheo', isAuth, (req, res, next) => {
+
+    console.log('-=-=-=-', req.body, req.user);
     let a = req.user.theologiansPicked.filter(theo => theo._id === req.body.object._id)[0];
     console.log(a, 'The user favorite list: ', req.user.theologiansPicked)
     let newList = a ?
@@ -18,5 +19,9 @@ router.post('/disptheo', (req, res, next) => {
         .then(user => {
             res.status(200).json(user);
         })
+
 });
+function isAuth(req, res, next) {
+    req.isAuthenticated() ? next() : res.status(401).json({ msg: 'Log in first' });
+}
 module.exports = router;
